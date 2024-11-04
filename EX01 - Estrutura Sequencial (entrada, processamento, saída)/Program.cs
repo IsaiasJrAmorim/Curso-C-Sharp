95592b65ab1c2645eb2fb7ceea8e0dbd187033d7
Isaias Junior Anacleto Amorim <isaiasjunioranacletoamorim@hotmail.com>
2024-11-04 16:04:43 -0300

    inserindo comentario

--- EX01 - Estrutura Sequencial (entrada, processamento, saída)/Program.cs
+++ EX01 - Estrutura Sequencial (entrada, processamento, saída)/Program.cs
@@ -1,7 +1,7 @@
 ﻿namespace EX01___Estrutura_Sequencial__entrada__processamento__saída_ {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!"); //Teste
         }
     }
 }
