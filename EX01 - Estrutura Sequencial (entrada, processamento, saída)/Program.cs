03cd586177a38d22c4e8876a886af004e6321ca7
Isaias Junior Anacleto Amorim <isaiasjunioranacletoamorim@hotmail.com>
2024-11-04 16:26:58 -0300

    Resolução

--- EX01 - Estrutura Sequencial (entrada, processamento, saída)/Program.cs
+++ EX01 - Estrutura Sequencial (entrada, processamento, saída)/Program.cs
@@ -1,7 +1,22 @@
 ﻿namespace EX01___Estrutura_Sequencial__entrada__processamento__saída_ {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!"); //Teste
+
+            // Leitura dos valores inteiros
+            Console.WriteLine("Digite o primeiro valor:");
+            int valor1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo valor:");
+            int valor2 = int.Parse(Console.ReadLine());
+
+            // Cálculo da soma
+            int soma = valor1 + valor2;
+
+            // Exibição do resultado
+            Console.WriteLine("SOMA = " + soma);
+
+            // Espera até que uma tecla seja pressionada
+            Console.WriteLine("Pressione qualquer tecla para continuar...");
+            Console.ReadKey();
         }
     }
 }
