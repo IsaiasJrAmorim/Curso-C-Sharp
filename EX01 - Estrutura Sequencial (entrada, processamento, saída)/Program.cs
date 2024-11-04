293345d5a67497e3419512c0e7d28b6815739ebe
Isaias Junior Anacleto Amorim <isaiasjunioranacletoamorim@hotmail.com>
2024-11-04 16:01:26 -0300

    Push

---
+++ EX01 - Estrutura Sequencial (entrada, processamento, saída)/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace EX01___Estrutura_Sequencial__entrada__processamento__saída_ {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
